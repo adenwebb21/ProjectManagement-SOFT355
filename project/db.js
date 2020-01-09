398ee81255252563ad5eb540ac9dc5bb4f306b1b
Aden Webb <adenwebb21@gmail.com>
2020-01-09 14:52:14 +0000

    removed unecessary files

--- project/db.js
+++
@@ -1,9 +0,0 @@
-var schemas = require("./schemas");
-
-async function getTasks()
-{
-  //await schemas.Task.findOne({"id": id});
-  await schemas.Task.find();
-}
-
-module.exports.getTasks = getTasks;
--- project/schemas.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require("mongoose");
-
-var Task = mongoose.model("Task", {id: Number, title: String, desc: String, due: Number, priority: Number}); // number will be converted to a date later
-
-module.exports.Task = Task;
